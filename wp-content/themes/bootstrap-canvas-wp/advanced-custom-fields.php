5b305f4f590bd27a2b73db47cb9fea70276e8b77
Dan Dan Ahrendt <daniel@gosolid.net>
2018-08-21 17:16:18 -0700

    Added submenu include to advanced custom fields file

--- wp-content/themes/bootstrap-canvas-wp/advanced-custom-fields.php
+++ wp-content/themes/bootstrap-canvas-wp/advanced-custom-fields.php
@@ -1,35 +1,29 @@
 <?php
-
 // check if the flexible content field has rows of data
 if( have_rows('modules') ){
 
-     // loop through the rows of data
-    while ( have_rows('modules') ) : the_row();
+  // loop through the rows of data
+  while ( have_rows('modules') ) : the_row();
+    if( get_row_layout() == 'submenu' ){
+      include 'submenu-module.php';
+    }elseif( get_row_layout() == 'full_width_hero'){
+      include 'full-width-hero.php';
+    }elseif( get_row_layout() == '1_2_hero'){
+      include '1-2-hero.php';
+    }elseif( get_row_layout() == '2_1_hero'){
+      include '2-1-hero.php';
+    }elseif( get_row_layout() == '1_1_1_hero'){
+      include '1-1-1-hero.php';
+    }elseif( get_row_layout() == '1_1_hero'){
+      include '1-1-hero.php';
+    }elseif( get_row_layout() == '1_1_1_1_hero'){
+      include '1-1-1-1-hero.php';
+    }elseif( get_row_layout() == 'staff_module'){
+      include 'staff-module-slide.php';
+    }elseif( get_row_layout() == 'basic_page_content'){
+      include 'basic-page-content.php';
+    }elseif( get_row_layout() == 'blog_roll'){
+        include 'blog-roll-logic.php';
 
-if( get_row_layout() == 'full_width_hero'){
-  include 'full-width-hero.php';
-}elseif( get_row_layout() == '1_2_hero'){
-  include '1-2-hero.php';
-}elseif( get_row_layout() == '2_1_hero'){
-  include '2-1-hero.php';
-}elseif( get_row_layout() == '1_1_1_hero'){
-  include '1-1-1-hero.php';
-}elseif( get_row_layout() == '1_1_hero'){
-  include '1-1-hero.php';
-}elseif( get_row_layout() == '1_1_1_1_hero'){
-  include '1-1-1-1-hero.php';
-}elseif( get_row_layout() == 'staff_module'){
-  include 'staff-module-slide.php';
-}elseif( get_row_layout() == 'basic_page_content'){
-  include 'basic-page-content.php';
-}elseif( get_row_layout() == 'blog_roll'){
-    include 'blog-roll-logic.php';
-
-}endwhile;
+  } endwhile;
  } ?>
-
-
-
-
-
-
