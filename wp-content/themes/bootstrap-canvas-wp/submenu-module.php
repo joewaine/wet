0157d707a0a02a3a7d7f5db24afff1f424f34450
Dan Dan Ahrendt <daniel@gosolid.net>
2018-08-21 17:09:26 -0700

    Submenu module template

---
+++ wp-content/themes/bootstrap-canvas-wp/submenu-module.php
@@ -0,0 +1,25 @@
+<?php
+
+// vars
+$submenu = get_sub_field('anchor_repeater');
+
+if( $submenu ){
+?>
+
+<div class="submenu full-width black">
+	<ul class="id-nav">
+
+		<?php foreach ($submenu as &$value) {?>
+
+			<li class="scroll-anchor" data="#<?php echo $value['slug_text']; ?>">
+			  <?php echo $value['text']; ?>
+			</li>
+
+		<?php  }?>
+
+	</ul>
+</div>
+
+<?php
+}?>
+
